b2d9cbebc139a8453c53153b98f41c1010b5b1df
nmjc208-stack <nmjc208@gmail.com>
2025-12-30 12:27:52 +0200

    Create init_database.sql to set up Datawarehouse

---
+++ scripts/init_database.sql
@@ -0,0 +1,13 @@
+-- Create Database 'Datawarehouse'
+
+USE master;
+
+CREATE DATABASE Datawarehouse;
+
+USE Datawarehouse;
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO 
+CREATE SCHEMA gold;
